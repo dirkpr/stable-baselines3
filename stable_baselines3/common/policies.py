db494ebd5b895b289d333f694b7ab8c6362094cc
Dirk Reinhardt <dirk.p.reinhardt@ntnu.no>
2023-11-08 08:30:09 +0100

    stable_baselines3/common/policies.py::class MPCActorCriticPolicy: Implement functions to interact with gymnasium environment.

--- stable_baselines3/common/policies.py
+++ stable_baselines3/common/policies.py
@@ -1124,3 +1124,86 @@ class MPCActorCriticPolicy(BasePolicy):
         action = th.tensor(action, dtype=th.float32)
 
         return action
+
+    def forward(
+        self, obs: th.Tensor, deterministic: bool = False
+    ) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
+        """
+        Forward pass in all the networks (actor and critic)
+
+        :param obs: Observation
+        :param deterministic: Whether to sample or use deterministic actions
+        :return: action, value and log probability of the action
+        """
+
+        # # Preprocess the observation if needed
+        # features = self.extract_features(obs)
+
+        # if self.share_features_extractor:
+        #     latent_pi, latent_vf = self.mlp_extractor(features)
+        # else:
+        #     pi_features, vf_features = features
+        #     latent_pi = self.mlp_extractor.forward_actor(pi_features)
+        #     latent_vf = self.mlp_extractor.forward_critic(vf_features)
+        # # Evaluate the values for the given observations
+
+        # values = self.value_net(latent_vf)
+
+        # distribution = self._get_action_dist_from_latent(latent_pi)
+
+        # actions = distribution.get_actions(deterministic=deterministic)
+
+        # log_prob = distribution.log_prob(actions)
+
+        # actions = actions.reshape((-1, *self.action_space.shape))
+        # return actions, values, log_prob
+
+        # Throw error if called
+        raise NotImplementedError("forward not implemented for MPCActorCriticPolicy")
+
+    def evaluate_actions(
+        self, obs: th.Tensor, actions: th.Tensor
+    ) -> Tuple[th.Tensor, th.Tensor, Optional[th.Tensor]]:
+        """
+        Evaluate actions according to the current policy,
+        given the observations.
+
+        :param obs: Observation
+        :param actions: Actions
+        :return: estimated value, log likelihood of taking those actions
+            and entropy of the action distribution.
+        """
+        # Preprocess the observation if needed
+        # features = self.extract_features(obs)
+        # if self.share_features_extractor:
+        #     latent_pi, latent_vf = self.mlp_extractor(features)
+        # else:
+        #     pi_features, vf_features = features
+        #     latent_pi = self.mlp_extractor.forward_actor(pi_features)
+        #     latent_vf = self.mlp_extractor.forward_critic(vf_features)
+        # distribution = self._get_action_dist_from_latent(latent_pi)
+        # log_prob = distribution.log_prob(actions)
+        # values = self.value_net(latent_vf)
+        # entropy = distribution.entropy()
+        # return values, log_prob, entropy
+
+        # Throw error if called
+        raise NotImplementedError(
+            "evaluate_actions not implemented for MPCActorCriticPolicy"
+        )
+
+    def predict_values(self, obs: th.Tensor) -> th.Tensor:
+        """
+        Get the estimated values according to the current policy given the observations.
+
+        :param obs: Observation
+        :return: the estimated values.
+        """
+        # features = super().extract_features(obs, self.vf_features_extractor)
+        # latent_vf = self.mlp_extractor.forward_critic(features)
+        # return self.value_net(latent_vf)
+
+        # Throw error if called
+        raise NotImplementedError(
+            "predict_values not implemented for MPCActorCriticPolicy"
+        )
